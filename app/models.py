ee16ba24ddbd92ed8b466d0655180e2dd3c6471d
JigSaW <misho_1998@mail.ru>
2021-11-29 13:23:54 +0300

    add user model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 from datetime import date
 
 
 class Books(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	author = models.CharField(max_length=50)
 	title = models.CharField(max_length=100)
 	date = models.DateField(default=date.today)
@@ -15,6 +17,7 @@ class Books(models.Model):
 		return f'{self.title}'
 
 class Courses(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	place = models.CharField(max_length=50)
 	title = models.CharField(max_length=100)
 	date = models.DateField(default=date.today)
@@ -25,4 +28,3 @@ class Courses(models.Model):
 
 	def __str__(self):
 		return f'{self.title}'
-
