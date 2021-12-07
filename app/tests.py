71337e96c8bdd121e4fcde30d6b8589097e6b8c2
JigSaW <misho_1998@mail.ru>
2021-12-08 01:02:39 +0300

    add test books

--- app/tests.py
+++ app/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model, authenticate
+from .models import Books
+from datetime import date, timedelta
 
 class SigninTest(TestCase):
 	def setUp(self):
@@ -20,3 +22,34 @@ class SigninTest(TestCase):
 	def test_wrong_pssword(self):
 		user = authenticate(username='test', password='wrong')
 		self.assertFalse(user is not None and user.is_authenticated)
+
+
+class BooksTest(TestCase):
+	def setUp(self):
+		self.user = get_user_model().objects.create_user(username='test', password='12test12', email='test@example.com')
+		self.user.save()
+		self.timestamp = date.today()
+		self.book = Books(user=self.user,
+						 author='Томас Кормен',
+						 title='Алгоритмы: построение и анализ',
+						 date=self.timestamp + timedelta(days=1))
+		self.book.save()
+
+	def tearDown(self):
+		self.user.delete()
+
+	def test_read_task(self):
+		self.assertEqual(self.book.user, self.user)
+		self.assertEqual(self.book.author, 'Томас Кормен')
+		self.assertEqual(self.book.title, 'Алгоритмы: построение и анализ')
+		self.assertEqual(self.book.date, self.timestamp + timedelta(days=1))
+
+	def test_update_task_description(self):
+		self.book.author = 'Чарльз Эрик Лейзерсон, Клиффорд Штайн'
+		self.book.save()
+		self.assertEqual(self.book.author, 'Чарльз Эрик Лейзерсон, Клиффорд Штайн')
+
+	def test_update_task_due(self):
+		self.book.date = self.timestamp + timedelta(days=2)
+		self.book.save()
+		self.assertEqual(self.book.date, self.timestamp + timedelta(days=2))
