47ab762bae1a65a465a7725530eea866ff8a8b13
JigSaW <misho_1998@mail.ru>
2021-12-20 23:27:00 +0300

    add new models, Want

--- app/models.py
+++ app/models.py
@@ -16,6 +16,18 @@ class Books(models.Model):
 	def __str__(self):
 		return f'{self.title}'
 
+class WantBooks(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.CharField(max_length=50)
+	title = models.CharField(max_length=100)
+
+	class Meta:
+		verbose_name = 'Книга, которую буду читать'
+		verbose_name_plural = 'Книги, которые буду читать'
+
+	def __str__(self):
+		return f'{self.title}'
+
 class Courses(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	place = models.CharField(max_length=50)
@@ -25,6 +37,18 @@ class Courses(models.Model):
 	class Meta:
 		verbose_name = 'Курс'
 		verbose_name_plural = 'Курсы'
+
+	def __str__(self):
+		return f'{self.title}'
+
+class WantCourses(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	place = models.CharField(max_length=50)
+	title = models.CharField(max_length=100)
+
+	class Meta:
+		verbose_name = 'Курс, который буду проходить'
+		verbose_name_plural = 'Курсы, которые буду проходить'
 
 	def __str__(self):
 		return f'{self.title}'
@@ -42,6 +66,18 @@ class Videos(models.Model):
 	def __str__(self):
 		return f'{self.title}'
 
+class WantVideos(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=100)
+	link = models.TextField()
+
+	class Meta:
+		verbose_name = 'Видео, которое буду смотреть'
+		verbose_name_plural = 'Видео, которые буду смотреть'
+
+	def __str__(self):
+		return f'{self.title}'
+
 class Articles(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
@@ -51,6 +87,18 @@ class Articles(models.Model):
 	class Meta:
 		verbose_name = 'Статья'
 		verbose_name_plural = 'Статьи'
+
+	def __str__(self):
+		return f'{self.title}'
+
+class WantArticles(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=100)
+	link = models.TextField()
+
+	class Meta:
+		verbose_name = 'Статья, которую буду читать'
+		verbose_name_plural = 'Статьи, которые буду читать'
 
 	def __str__(self):
 		return f'{self.title}'
